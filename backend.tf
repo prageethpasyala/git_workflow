335265234fbb47bd1ad5a22d50bb342866072c23
prageethpasyala <prageeth.pasyala@cloudreach.com>
2021-12-07 17:54:29 +0000

    git workflow v2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # S3 bucket to store your Terraform tfstates
 terraform {
   backend "s3" {
-    bucket = "talent-academy-439272626435-tfstates-pascal"
+    bucket = "cr-pkp-keys"
     key    = "terraform-test/terraform.tfstates" # KEY must be unique for each PROJECT
   }
 }
